4f57d96aa6dd94f39243dab67d8f59a5c2ac9134
Marina Ranenko <mranenko@gmail.com>
2020-10-11 14:10:17 -0600

    Todo list settings (initial commit)

--- src/app/pages/settings-page/settings-page.component.ts
+++ src/app/pages/settings-page/settings-page.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+/* app services */
+import {TodoService} from "../../services/todo.service";
+
 
 @Component({
   selector: 'app-settings-page',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings-page.component.scss']
 })
 export class SettingsPageComponent implements OnInit {
-
-  constructor() { }
+  constructor(public todoService: TodoService) {
+  }
 
   ngOnInit(): void {
   }
 
+
+  /**
+   * Event handlers
+   */
+
+  onSettingsChange(event: Event): void {
+    const eventTarget = <HTMLInputElement>event.target;
+    const settingName = eventTarget.name || '';
+    const settingValue = eventTarget.value;
+    const settingChecked = eventTarget.checked;
+
+    if (settingName) {
+      this.todoService.settings[settingName] = settingValue;
+      this.todoService.applySetting(settingName, settingValue);
+    }
+    else {
+      this.todoService.settings[settingValue] = settingChecked;
+      this.todoService.applySetting(settingValue, settingChecked);
+    }
+  }
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -9,41 +9,47 @@ import {StorageService} from './storage.service';
 })
 export class TodoService {
   readonly key = {
+    settings: 'settings',
     todoList: 'todo-list',
   };
 
+  readonly sortOrder = {
+    alphabetical: 'alphabetical',
+    newestFirst: 'newestFirst',
+    oldestFirst: 'oldestFirst',
+  };
+
+  settings: Settings = null;
   todoList: Todo[] = [];
 
   constructor(private storageService: StorageService) {
-    /* load list from storage (if exists) */
+    /* load data from storage (if exist) */
+    this.settings = JSON.parse(this.storageService.get(this.key.settings)) ||
+      {
+        incompleteFirst: true,
+        sortOrder: this.sortOrder.newestFirst,
+      };
+
     this.todoList = JSON.parse(this.storageService.get(this.key.todoList)) || [];
   }
 
+  applySettings(): void {
+    /* apply all of the specified todo list settings */
+    for (let [setting, value] of Object.entries(this.settings)) {
+      console.log(`${setting}: ${value}`);
+      this.applySetting(setting, value);
+    }
+  }
 
-  /*
-    Todo list manipulation
-   */
-
-  add(todo: string): void {
-    if (todo) {
-      this.todoList.push({
-        task: todo,
-        timeCreated: new Date(),
-        timeCompleted: null,
-      });
+  applySetting(setting, value): void {
+    if (setting === 'sortOrder') {
+      this.sort(value);
+    }
+    else if (setting === 'incompleteFirst') {
+      this.showIncompleteFirst(value);
     }
 
-    this.saveList();
-  }
-
-  clearAll(): void {
-    this.todoList = [];
-    this.saveList();
-  }
-
-  clearCompleted(): void {
-    this.todoList = this.todoList.filter(todo => (todo.timeCompleted === null));
-    this.saveList();
+    this.saveSettings();
   }
 
   toggleCompleted(todoIndex: number): void {
@@ -52,8 +58,36 @@ export class TodoService {
         (this.todoList[todoIndex].timeCompleted === null) ?
           new Date() : null;
 
-      this.saveList();
+      this.applySettings();
     }
+  }
+
+
+  /*
+    Todo list manipulation and sorting
+   */
+
+  add(todo: string): void {
+    if (todo) {
+      this.todoList.unshift({
+        task: todo,
+        timeCreated: new Date(),
+        timeCompleted: null,
+      });
+    }
+
+    this.applySettings();
+    this.saveTodo();
+  }
+
+  clearAll(): void {
+    this.todoList = [];
+    this.saveTodo();
+  }
+
+  clearCompleted(): void {
+    this.todoList = this.todoList.filter(todo => (todo.timeCompleted === null));
+    this.saveTodo();
   }
 
 
@@ -61,19 +95,70 @@ export class TodoService {
     Private methods
    */
 
-  private saveList(): void {
+  private saveSettings(): void {
+    /* save settings to storage */
+    this.storageService.set(this.key.settings, JSON.stringify(this.settings));
+  }
+
+  private saveTodo(): void {
     /* save list to storage */
     this.storageService.set(this.key.todoList, JSON.stringify(this.todoList));
+  }
+
+  private showIncompleteFirst(incompleteFirst: boolean): void {
+    if (incompleteFirst && this.todoList.length > 0) {
+      let incompleteTodoList = this.todoList.filter(todo => (todo.timeCompleted === null));
+      let completedTodoList = this.todoList.filter(todo => (todo.timeCompleted !== null));
+
+      if (incompleteTodoList.length > 0 && completedTodoList.length > 0) {
+        console.log(incompleteTodoList.concat(completedTodoList));
+        this.todoList = incompleteTodoList.concat(completedTodoList);
+      }
+    }
+    else if (!incompleteFirst && this.todoList.length > 0) {
+      this.sort(this.settings.sortOrder);
+    }
+
+    this.saveTodo();
+  }
+
+  private sort(sortOrder: string): void {
+    const dateComparator = (a, b) => {
+      return new Date(a).getTime() - new Date(b).getTime();
+    };
+
+    const stringComparator = (a, b) => {
+      a = a.toLowerCase();
+      b = b.toLowerCase();
+      return (a < b) ? -1 : (a > b) ? 1 : 0;
+    };
+
+    if (sortOrder === this.sortOrder.alphabetical) {
+      this.todoList.sort((a, b) => stringComparator(a.task, b.task));
+    }
+    else if (sortOrder === this.sortOrder.newestFirst) {
+      this.todoList.sort((a, b) => dateComparator(b.timeCreated, a.timeCreated));
+    }
+    else if (sortOrder === this.sortOrder.oldestFirst) {
+      this.todoList.sort((a, b) => dateComparator(a.timeCreated, b.timeCreated));
+    }
+
+    this.saveTodo();
   }
 }
 
 
 /*
-  Data model
+  Data models
  */
 
 interface Todo {
   task: string;
   timeCreated: Date;
   timeCompleted: Date;
+}
+
+interface Settings {
+  incompleteFirst: boolean;
+  sortOrder: string;
 }
